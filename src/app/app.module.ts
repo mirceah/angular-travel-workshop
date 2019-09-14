60df626cd62828495485e51423e723c72bdbc530
mirceah <mircea.hasegan@gmail.com>
2019-09-14 15:42:41 +0300

    task2 7.3 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { MatIconModule } from "@angular/material";
 import { AppComponent } from './app.component';
 import { FavoriteDestinationComponent } from './favorite-destination/favorite-destination.component';
 import { DestinationDetailsComponent } from './destination-details/destination-details.component';
+import { DestinationSummaryComponent } from './destination-summary/destination-summary.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MatIconModule ],
-  declarations: [ AppComponent, FavoriteDestinationComponent, DestinationDetailsComponent ],
+  declarations: [ AppComponent, FavoriteDestinationComponent, DestinationDetailsComponent, DestinationSummaryComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
