b8f064666417c3d096c8f6b3a94db3086b2c6ae5
mirceah <mircea.hasegan@gmail.com>
2019-09-15 19:05:03 +0300

    task 9 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from "@angular/material";
-// import { RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FavoriteDestinationComponent } from './favorite-destination/favorite-destination.component';
@@ -14,11 +14,11 @@ import { DestinationsComponent } from './destinations/destinations.component';
 @NgModule({
   imports:      [
     BrowserModule, FormsModule, MatIconModule,
-    // RouterModule.forRoot([
-    //   { path: 'destinations', component: DestinationsComponent },
-    //   // route to contact component
-    //   { path: '', redirectTo: '/destinations', pathMatch: 'full' }
-    // ])
+    RouterModule.forRoot([
+      { path: 'destinations', component: DestinationsComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '', redirectTo: '/destinations', pathMatch: 'full' }
+    ])
     ],
   declarations: [ AppComponent, FavoriteDestinationComponent, DestinationDetailsComponent, DestinationSummaryComponent, ContactComponent, DestinationsComponent ],
   bootstrap:    [ AppComponent ]
